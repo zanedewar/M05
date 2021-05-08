f39d758956e9c477da07af682eb6ca5bc08c431a
zanedewar <zane_dewar@hotmail.com>
2021-05-08 04:17:49 -0700

    File setup
    Setting up html, css, and js files.

---
+++ M05js.js
@@ -0,0 +1,11 @@
+var table = prompt();             // Unit of table
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+
+for(var i = 1; i < 11; i++) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+ }
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
